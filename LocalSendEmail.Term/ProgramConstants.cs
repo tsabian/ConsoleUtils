6ef5c8bc9e9c3be78099541c143261657c6eb991
Tiago Oliveira <tiago@bitxlabs.com.br>
2021-05-14 19:11:13 -0300

    Update ProgramConstants.cs

--- LocalSendEmail.Term/ProgramConstants.cs
+++ LocalSendEmail.Term/ProgramConstants.cs
@@ -4,7 +4,7 @@
     {
         internal const string SmtpHost = "smtp.gmail.com";
         internal const int SmtpPort = 587;
-        internal const string SmtpUserName = "tiagoo@ciandt.com";
+        internal const string SmtpUserName = "email@email";
         internal const string SmtpUserPassword = "xtwdnlhntcjcmjbs";
         internal const bool UseHtmlBody = true;
         internal const bool UseDefaultCredentials = false;
